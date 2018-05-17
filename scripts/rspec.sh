10e12476632ec139d8e4719d9c388fea4e0f5971
Maxim <mlavrenenko@strongqa.com>
2018-05-17 16:27:33 +0300

    added downloading of chromedriver

--- scripts/rspec.sh
+++ scripts/rspec.sh
@@ -2,4 +2,8 @@
 set -ev
 cd ./howitzer_rspec
 bundle install
+wget https://chromedriver.storage.googleapis.com/2.33/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo cp chromedriver /usr/local/bin/chromedriver
+sleep 3
 bundle exec rake rspec
